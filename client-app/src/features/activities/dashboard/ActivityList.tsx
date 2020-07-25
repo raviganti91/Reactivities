9d4706bd1c394399a8fbc64117df5bc372294b05
Ravi Teja Ganti <raganti@microsoft.com>
2020-07-25 19:08:07 +0530

    End of section 13

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,31 +1,28 @@
-import React, { useContext, Fragment } from 'react'
-import { Item, Label } from 'semantic-ui-react'
+import React, { useContext, Fragment } from 'react';
+import { Item, Label } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
-import ActivityStore from '../../../app/stores/activityStore';
-import { ActivityListItem } from '../dashboard/ActivityListItem';
+import {ActivityListItem} from './ActivityListItem';
+import { RootStoreContext } from '../../../app/stores/rootStore';
 
- const ActivityList :React.FC= () => {
-    const activityStore = useContext(ActivityStore);
-    const {activitiesByDate} = activityStore;
-    return (
-        <Fragment>
-            {activitiesByDate.map(([group,activities])=>(
-                <Fragment  key={group}>
-                <Label  size='large' color='blue'>
-                    {group}
-                </Label>
-                 
-                 <Item.Group divided>
-                     {activities.map(activity =>(
-                        <ActivityListItem key={activity.id} activity ={activity}/>
-                     ))}
-                    
-                   </Item.Group>
-            
-            </Fragment>
+const ActivityList: React.FC = () => {
+  const rootStore = useContext(RootStoreContext);
+  const { activitiesByDate } = rootStore.activityStore;
+  return (
+    <Fragment>
+      {activitiesByDate.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Label size='large' color='blue'>
+            {group}
+          </Label>
+          <Item.Group divided>
+            {activities.map(activity => (
+              <ActivityListItem key={activity.id} activity={activity} />
             ))}
+          </Item.Group>
         </Fragment>
-       
-    )
+      ))}
+    </Fragment>
+  );
 };
+
 export default observer(ActivityList);
